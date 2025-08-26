707d54b5f399591161ac820bdc11b576ca15b3a0
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 22:45:59 +0200

    add

--- src/routes/seminar/+page.svelte
+++ src/routes/seminar/+page.svelte
@@ -1,5 +1,29 @@
 <div class=main>
-  <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>
+In the spring of 2023, we launched a seminar series titled Quantum Algorithms, where you can learn about the world of quantum computing through lectures on a variety of topics, ranging from established findings to ongoing research, delivered by both students and researchers.
+</p>
+
+<p>
+Chair: <a href="https://cs.bme.hu/~friedl">Katalin Friedl</a>, <a href="http://gilyen.hu">András Gilyén</a>
+</p>
+
+- 2024-05-16: [Quantum cryptanalysis of isogeny-based cryptography]({{ site.baseurl }}/posts/2024-05-16-quantum-cryptanalysis-of-isogeny-based-cryptography)
+- 2024-04-11: [Near-Optimal Quantum Algorithms for Multivariate Mean Estimation]({{ site.baseurl }}/posts/2024-04-11-multivariate-mean-estimation)
+- 2024-03-21: [Unitary Complexity and the Uhlmann Transformation Problem]({{ site.baseurl }}/posts/2024-03-21-qip-2024)
+- 2023-12-04: [Kvantumáramkörök hatékony szimulációja Fermionikus Lineáris Optikával]({{ site.baseurl }}/posts/2023-12-04-majorana-szimulator)
+- 2023-11-27: [Clifford-áramkörök szimulációja Heisenberg-képben, és pár szó a fermionokról]({{ site.baseurl }}/posts/2023-11-27-clifford-szimulator)
+- 2023-11-20: [Elfs, trees and quantum walks]({{ site.baseurl }}/posts/2023-11-20-elfs-trees-and-quantum-walks)
+- 2023-11-06: [Iterative refinement for quantum tomography]({{ site.baseurl }}/posts/2023-11-06-iterative-refinement-for-quantum-tomography)
+- 2023-09-25: [Kvantumbitek gazdag dinamikája]({{ site.baseurl }}/posts/2023-09-25-kvantumbitek-gazdag-dinamikaja)
+- 2023-05-26: [Kvantumalgoritmusok az izogénia-alapú kriptográfiában]({{ site.baseurl }}/posts/2023-05-26-kvantumalgoritmusok-az-izogenia-alapu-kriptografiaban)
+- 2023-05-19: [Kvantumos bonyolultsági osztályok]({{ site.baseurl }}/posts/2023-05-19-kvantumos-bonyolultsagi-osztalyok)
+- 2023-05-12: [Quantum Thermal State Preparation]({{ site.baseurl }}/posts/2023-05-12-quantum-thermal-state-preparation)
+- 2023-04-28: [Összefonódás és ami mögötte van]({{ site.baseurl }}/posts/2023-04-28-osszefonodas-es-ami-mogotte-van)
+- 2023-04-21: [Kvantumos jelfeldolgozás és szingulárisérték transzformáció]({{ site.baseurl }}/posts/2023-04-21-kvantumos-jelfeldolgozas-es-szingularisertek-transzformacio)
+- 2023-04-14: [A kvantumos algoritmusok egyesített elmélete: kvantumos szingulárisérték-transzformáció]({{ site.baseurl }}/posts/2023-04-14-kvantumos-szingularisertek-transzformacio)
+- 2023-03-31: [Mintavételezés kvantumos bolyongásokkal]({{ site.baseurl }}/posts/2023-03-31-mintavetelezes-kvantumos-bolyongasokkal)
+- 2023-03-17: [Bevezetés a Szegedy-féle kvantumbolyongásokba]({{ site.baseurl }}/posts/2023-03-17-bevezetes-a-szegedy-fele-kvantumbolyongasokba)
 
 </div>
 
