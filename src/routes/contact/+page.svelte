08103988b834c736830002a1f0060468a1e21e4a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 23:39:57 +0200

    add

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,65 @@
+<div class="main">
+
+  <div class="person">
+    <h3>Katalin Friedl</h3>
+    <p>Associate Professor</p>
+    <p>
+      <a href="https://www.szit.bme.hu/english/about_us/contact.html">IB137/a</a><br>
+      <a href="mailto:friedl@cs.bme.hu">friedl(at)cs.bme.hu</a><br>
+      <a href="https://cs.bme.hu/~friedl/indexen.html">cs.bme.hu/~friedl</a>
+    </p>
+  </div>
+
+  <div class="person">
+    <h3>László Kabódi</h3>
+    <p>Assistant Lecturer</p>
+    <p>
+      <a href="https://www.szit.bme.hu/english/about_us/contact.html">IB137/a</a><br>
+      <a href="mailto:kabodil@gmail.com">kabodil(at)gmail.com</a>
+    </p>
+  </div>
+
+  <div class="person">
+    <h3>Viktória Nemkin</h3>
+    <p>PhD Student</p>
+    <p>
+      <a href="https://www.szit.bme.hu/english/about_us/contact.html">IE216/3</a><br>
+      <a href="mailto:nemkin@cs.bme.hu">nemkin(at)cs.bme.hu</a><br>
+      <a href="https://cs.bme.hu/~nemkin">cs.bme.hu/~nemkin</a>
+    </p>
+  </div>
+
+</div>
+
+<style>
+  .main {
+    margin: 1rem;
+    font-family: sans-serif;
+  }
+
+  .person {
+    margin-bottom: 1.5rem;
+    padding: 1rem;
+    border-left: 4px solid #444;
+    background: #f9f9f9;
+    border-radius: 6px;
+  }
+
+  .person h3 {
+    margin: 0 0 0.25rem 0;
+  }
+
+  .person p {
+    margin: 0.25rem 0;
+    line-height: 1.4;
+  }
+
+  .person a {
+    color: #0056a3;
+    text-decoration: none;
+  }
+
+  .person a:hover {
+    text-decoration: underline;
+  }
+</style>
