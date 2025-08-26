e4a6f4a0a2c87a30cfda631969e22111a81a56ca
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 23:49:19 +0200

    add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script>
   import Header from '$lib/Header.svelte';
-	import favicon from '$lib/icons/favicon.png';
+  import favicon from '$lib/icons/favicon.png';
   import '../global.scss';
-	let { children } = $props();
 </script>
 
 <svelte:head>
   <title>QuSZIT</title>
   <meta name="description" content="QuSZIT - Quantum algorithms at BME VIK SZIT" />
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href="{favicon}" />
 </svelte:head>
 
 <Header />
 
-{@render children?.()}
+<slot />
