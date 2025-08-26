f4c456dfeb3632ca49763290847546b0fa72f03a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 22:55:50 +0200

    add

--- src/routes/seminar/+page.svelte
+++ src/routes/seminar/+page.svelte
@@ -8,22 +8,100 @@ In the spring of 2023, we launched a seminar series titled Quantum Algorithms, w
 Chair: <a href="https://cs.bme.hu/~friedl">Katalin Friedl</a>, <a href="http://gilyen.hu">András Gilyén</a>
 </p>
 
-- 2024-05-16: [Quantum cryptanalysis of isogeny-based cryptography]({{ site.baseurl }}/posts/2024-05-16-quantum-cryptanalysis-of-isogeny-based-cryptography)
-- 2024-04-11: [Near-Optimal Quantum Algorithms for Multivariate Mean Estimation]({{ site.baseurl }}/posts/2024-04-11-multivariate-mean-estimation)
-- 2024-03-21: [Unitary Complexity and the Uhlmann Transformation Problem]({{ site.baseurl }}/posts/2024-03-21-qip-2024)
-- 2023-12-04: [Kvantumáramkörök hatékony szimulációja Fermionikus Lineáris Optikával]({{ site.baseurl }}/posts/2023-12-04-majorana-szimulator)
-- 2023-11-27: [Clifford-áramkörök szimulációja Heisenberg-képben, és pár szó a fermionokról]({{ site.baseurl }}/posts/2023-11-27-clifford-szimulator)
-- 2023-11-20: [Elfs, trees and quantum walks]({{ site.baseurl }}/posts/2023-11-20-elfs-trees-and-quantum-walks)
-- 2023-11-06: [Iterative refinement for quantum tomography]({{ site.baseurl }}/posts/2023-11-06-iterative-refinement-for-quantum-tomography)
-- 2023-09-25: [Kvantumbitek gazdag dinamikája]({{ site.baseurl }}/posts/2023-09-25-kvantumbitek-gazdag-dinamikaja)
-- 2023-05-26: [Kvantumalgoritmusok az izogénia-alapú kriptográfiában]({{ site.baseurl }}/posts/2023-05-26-kvantumalgoritmusok-az-izogenia-alapu-kriptografiaban)
-- 2023-05-19: [Kvantumos bonyolultsági osztályok]({{ site.baseurl }}/posts/2023-05-19-kvantumos-bonyolultsagi-osztalyok)
-- 2023-05-12: [Quantum Thermal State Preparation]({{ site.baseurl }}/posts/2023-05-12-quantum-thermal-state-preparation)
-- 2023-04-28: [Összefonódás és ami mögötte van]({{ site.baseurl }}/posts/2023-04-28-osszefonodas-es-ami-mogotte-van)
-- 2023-04-21: [Kvantumos jelfeldolgozás és szingulárisérték transzformáció]({{ site.baseurl }}/posts/2023-04-21-kvantumos-jelfeldolgozas-es-szingularisertek-transzformacio)
-- 2023-04-14: [A kvantumos algoritmusok egyesített elmélete: kvantumos szingulárisérték-transzformáció]({{ site.baseurl }}/posts/2023-04-14-kvantumos-szingularisertek-transzformacio)
-- 2023-03-31: [Mintavételezés kvantumos bolyongásokkal]({{ site.baseurl }}/posts/2023-03-31-mintavetelezes-kvantumos-bolyongasokkal)
-- 2023-03-17: [Bevezetés a Szegedy-féle kvantumbolyongásokba]({{ site.baseurl }}/posts/2023-03-17-bevezetes-a-szegedy-fele-kvantumbolyongasokba)
+<details>
+  <summary>2024. 05. 16. Péter Kutas: Quantum cryptanalysis of isogeny-based cryptography</summary>
+  <p>
+    Isogeny-based cryptography is  one of the newest branches of post-quantum cryptography. The main appeal of isogenies is that you can design a variety of schemes from them and they all require relatively small bandwidth. The talk will focus on quantum cryptanalysis of isogeny-based schemes. This will involve applications of hidden shift, hidden subgroup algorithms and connections to pathfinding in graphs. I will demonstrate the recent break of pSIDH and improved quantum algorithms for finding fixed degree isogenies and pose an array of open problems which do not require isogeny expertise.
+  </p>
+</details>
+
+<details>
+  <summary>2024. 04. 11. Jakab Csatári: Near-Optimal Quantum Algorithms for Multivariate Mean Estimation</summary>
+  <p><a href="https://arxiv.org/abs/2111.09787">https://arxiv.org/abs/2111.09787</a></p>
+  <p>
+    Based on the article of Arjan Cornelissen, Yassine Hamoudi and Sofiene Jerbi, we will look into the problem of estimating in Euclidean norm the mean of a vector-valued random variable with finite mean and covariance. Unlike classically, where any univariate estimator can be turned into a multivariate estimator with at most a logarithmic overhead in the dimension, no similar result can be proved in the quantum setting. Previously S. Heinrich ruled out the existence of a quantum advantage for the mean estimation problem when the sample complexity is smaller than the dimension.
+  </p>
+  
+  <a href="https://doi.org/10.1006/jcom.2001.0629">https://doi.org/10.1006/jcom.2001.0629</a>
+  
+  <p>
+    The main result of the authors is to show that, outside this low-precision regime, there is a quantum estimator that outperforms any classical estimator. Their approach is substantially more involved than in the univariate setting, where most quantum estimators rely only on phase estimation, exploiting a variety of additional algorithmic techniques such as amplitude amplification, the Bernstein-Vazirani algorithm, and quantum singular value transformation.
+  </p>
+</details>
+
+<details>
+  <summary>2024. 03. 21. Watch together: Tony Metger, QIP 2024, Unitary Complexity and the Uhlmann Transformation Problem</summary>
+  State transformation problems such as compressing quantum information or breaking quantum commitments are fundamental quantum tasks. However, their computational difficulty cannot easily be characterized using traditional complexity theory, which focuses on tasks with classical inputs and outputs.
+  
+  To study the complexity of such state transformation tasks, we introduce a framework for unitary synthesis problems, including notions of reductions and unitary complexity classes. We use this framework to study the complexity of transforming one entangled state into another via local operations. We formalize this as the Uhlmann Transformation Problem, an algorithmic version of Uhlmann's theorem. Then, we prove structural results relating the complexity of the Uhlmann Transformation Problem, polynomial space quantum computation, and zero knowledge protocols.
+  
+  The Uhlmann Transformation Problem allows us to characterize the complexity of a variety of tasks in quantum information processing, including decoding noisy quantum channels, breaking falsifiable quantum cryptographic assumptions, implementing optimal prover strategies in quantum interactive proofs, and decoding the Hawking radiation of black holes. Our framework for unitary complexity thus provides new avenues for studying the computational complexity of many natural quantum information processing tasks.
+</details>
+
+<details>
+  <summary>2023-12-04 — Kvantumáramkörök hatékony szimulációja Fermionikus Lineáris Optikával</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-11-27 — Clifford-áramkörök szimulációja Heisenberg-képben, és pár szó a fermionokról</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-11-20 — Elfs, trees and quantum walks</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-11-06 — Iterative refinement for quantum tomography</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-09-25 — Kvantumbitek gazdag dinamikája</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-05-26 — Kvantumalgoritmusok az izogénia-alapú kriptográfiában</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-05-19 — Kvantumos bonyolultsági osztályok</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-05-12 — Quantum Thermal State Preparation</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-04-28 — Összefonódás és ami mögötte van</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-04-21 — Kvantumos jelfeldolgozás és szingulárisérték transzformáció</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-04-14 — A kvantumos algoritmusok egyesített elmélete: kvantumos szingulárisérték-transzformáció</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-03-31 — Mintavételezés kvantumos bolyongásokkal</summary>
+  TODO
+</details>
+
+<details>
+  <summary>2023-03-17 — Bevezetés a Szegedy-féle kvantumbolyongásokba</summary>
+  TODO
+</details>
 
 </div>
 
