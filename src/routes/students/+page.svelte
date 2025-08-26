422c9256200c7c8ec135aef9b5335cba5070e853
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 20:05:26 +0200

    add

--- src/routes/students/+page.svelte
+++ src/routes/students/+page.svelte
@@ -7,9 +7,9 @@
 
 <div class=main>
 
-Project proposals
+<h2>Project proposals</h2>
 
-Quantum algorithms
+<h3>Quantum algorithms</h3>
 
 In the late 1970s, physicists proposed that since quantum mechanical phenomena could not be efficiently simulated with computers, perhaps a machine that also builds on quantum effects could solve certain problems faster. This idea gave birth to a new mathematical model of computation, the quantum Turing machine.
 
@@ -17,13 +17,13 @@ One of the most interesting capabilities of the quantum Turing machine is its ab
 
 The goal of the research is to examine existing algorithmic methods, develop them further, and investigate their applicability. For example to give quantum algorithmic equivalents to classical algorithms for bipartite matching, spanning trees, or network flows, and determining the extent to which they can be accelerated.
 
-Quadratic optimization on the adiabatic quantum computer
+<h3>Quadratic optimization on the adiabatic quantum computer</h3>
 
 QUBO stands for Quadratic Unconstrained Binary Optimization, which is an optimization technique more general than linear programming as it allows for quadratic terms in the objective function. Adiabatic quantum computers, which perform computations through quantum physical processes, are suitable for solving optimization tasks written in a QUBO format.
 
 An important question in this topic is how to efficiently represent more complex mathematical structures, such as graphs in this way. The difficulty lies in the practical constraint that the objective function cannot be arbitrarily complex, with a strict limitation on the number of allowed interactions (quadratic terms) between variables. These relationships can be represented as a graph, that must be embedded into the graph given by the hardware of the adiabatic quantum computer. Often, this can only be achieved by cleverly reformulating the objective function, introducing new variables and special terms.
 
-Quantum walks and search algorithms
+<h3>Quantum walks and search algorithms</h3>
 
 Classical random walks on graphs are used in numerous applications. They help model random processes and can help solve difficult problems in algorithm theory, that can be formulated as searches. Introducing randomness can provide faster (approximate) results and can be suitable for more efficient processing of larger inputs. For example, the "Boolean satisfiability problem," or SAT for short, is a well-known NP-complete problem, which can be formulated as a search: We can imagine all possible assignments of values to variables as the search space, i.e., the vertices of the graph. Then a possible step (an edge in the graph) could be the negation of a single variable's value. By stepping through this graph based on clever rules, an approximate solution can be found more quickly than with completely randomly generated candidates, and such solvers can perform well in practice.
 
