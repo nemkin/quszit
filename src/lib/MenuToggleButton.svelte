1369560c41692afa352f1a683a04264e552c122d
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-26 18:00:50 +0200

    add

--- src/lib/MenuToggleButton.svelte
+++ src/lib/MenuToggleButton.svelte
@@ -26,14 +26,16 @@
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 24 24" 
     fill="none" 
-    stroke="currentColor" 
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round" 
     aria-hidden="true"
     width=16px
     height=16px
   >
-    <line x1="3" y1="6" x2="21" y2="6" stroke-width="2" stroke-linecap="round"/>
-    <line x1="3" y1="12" x2="21" y2="12" stroke-width="2" stroke-linecap="round"/>
-    <line x1="3" y1="18" x2="21" y2="18" stroke-width="2" stroke-linecap="round"/>
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="18" x2="21" y2="18" />
   </svg>
 </button>
 
