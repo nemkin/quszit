1d888549154598f61fa72c6bf4277175f53319d8
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-29 21:36:41 +0200

    add

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -22,7 +22,7 @@
     <tbody>
       <tr class="table-row">
         <td data-label="Authors">Daróczy, Friedl, Kabódi, Pereszlényi, Szabó</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/dfkpsz-2021-quantum-adaptive-boosting.pdf" target="_blank">Quantum inspired adaptive boosting</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/dfkpsz-2021-quantum-adaptive-boosting.pdf">Quantum inspired adaptive boosting</a></td>
         <td data-label="Year">2021</td>
       </tr>
       
@@ -34,37 +34,37 @@
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-store-qft-quidd.pdf" target="_blank">Storing the quantum Fourier operator in the QuIDD data structure</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-store-qft-quidd.pdf">Storing the quantum Fourier operator in the QuIDD data structure</a></td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-embedding-chimera.pdf" target="_blank">Embedding logical functions into the Chimera graph</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-embedding-chimera.pdf">Embedding logical functions into the Chimera graph</a></td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2015-improve-quidd.pdf" target="_blank"> An idea to improve QuIDD based quantum simulations</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2015-improve-quidd.pdf"> An idea to improve QuIDD based quantum simulations</a></td>
         <td data-label="Year">2015</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2014-hidden-translation-translating-coset.pdf" target="_blank">Hidden Translation and Translating Coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2014-hidden-translation-translating-coset.pdf">Hidden Translation and Translating Coset in quantum computing</a></td>
         <td data-label="Year">2014</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fmss-2009-quantum-group-testers.pdf" target="_blank">Quantum testers for hidden group properties</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fmss-2009-quantum-group-testers.pdf">Quantum testers for hidden group properties</a></td>
         <td data-label="Year">2009</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2003-hidden-translation-orbit-coset.pdf" target="_blank">Hidden Translation and Orbit Coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2003-hidden-translation-orbit-coset.pdf">Hidden Translation and Orbit Coset in quantum computing</a></td>
         <td data-label="Year">2003</td>
       </tr>
       
--- src/routes/student-theses/+page.svelte
+++ src/routes/student-theses/+page.svelte
@@ -27,67 +27,67 @@
         <td data-label="Date">2022. 12. 18.</td>
         <td data-label="Student">Viktória Nemkin</td>
         <td data-label="Type"><span class="type msc">MSc</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2022-quantum-bioinformatics-msc.pdf" target="_blank">Application of quantum computing in Bioinformatics</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2022-quantum-bioinformatics-msc.pdf">Application of quantum computing in Bioinformatics</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2022. 11. 17.</td>
         <td data-label="Student">Viktória Nemkin</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2022-memory-quantum-simulation-tdk.pdf" target="_blank">Optimizing memory usage in quantum algorithm simulation</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2022-memory-quantum-simulation-tdk.pdf">Optimizing memory usage in quantum algorithm simulation</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2021. 12. 18.</td>
         <td data-label="Student">Márton Zalavári</td>
         <td data-label="Type"><span class="type msc">MSc</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/zalavari-2021-kozelito-vagasok-msc.pdf" target="_blank">Módszerek közel optimális vágások keresésére</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/zalavari-2021-kozelito-vagasok-msc.pdf">Módszerek közel optimális vágások keresésére</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2021. 11. 16.</td>
         <td data-label="Student">Márton Zalavári</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/zalavari-2021-qubo-tdk.pdf" target="_blank">Kvadratikus optimalizálás kvantum alapú számítógéppel</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/zalavari-2021-qubo-tdk.pdf">Kvadratikus optimalizálás kvantum alapú számítógéppel</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2021. 11. 16.</td>
         <td data-label="Student">Viktória Nemkin</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2021-simulate-qwalk-tdk.pdf" target="_blank">Simulation of quantum walks on a classical computer</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/nemkin-2021-simulate-qwalk-tdk.pdf">Simulation of quantum walks on a classical computer</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2021. 05. 21.</td>
         <td data-label="Student">Dániel Szabó</td>
         <td data-label="Type"><span class="type msc">MSc</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2021-tulajdonsag-teszteles-msc.pdf" target="_blank">Függvény és gráftulajdonságok lokális tesztelése</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2021-tulajdonsag-teszteles-msc.pdf">Függvény és gráftulajdonságok lokális tesztelése</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2018. 12. 06.</td>
         <td data-label="Student">Dániel Szabó</td>
         <td data-label="Type"><span class="type bsc">BSc</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2018-kvantum-gepi-tanulas-bsc.pdf" target="_blank">Kvantuminformatika és gépi tanulás</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2018-kvantum-gepi-tanulas-bsc.pdf">Kvantuminformatika és gépi tanulás</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2018. 11. 14.</td>
         <td data-label="Student">Dániel Szabó</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2018-gepi-tanulas-kvantum-tdk.pdf" target="_blank">Gépi tanulás és kvantuminformatika</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2018-gepi-tanulas-kvantum-tdk.pdf">Gépi tanulás és kvantuminformatika</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2017. 11. 16.</td>
         <td data-label="Student">Dániel Szabó</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2017-grafelm-dwave-tdk.pdf" target="_blank">Gráfelméleti algoritmusok beágyazása D-Wave kvantumszámítógépbe</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/szabo-2017-grafelm-dwave-tdk.pdf">Gráfelméleti algoritmusok beágyazása D-Wave kvantumszámítógépbe</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2014. 12. 29.</td>
         <td data-label="Student">László Kabódi</td>
         <td data-label="Type"><span class="type msc">MSc</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/kabodi-2014-kvantum-bolyongas-msc.pdf" target="_blank">Kvantumalgoritmusok véletlen bolyongással</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/kabodi-2014-kvantum-bolyongas-msc.pdf">Kvantumalgoritmusok véletlen bolyongással</a></td>
       </tr>
       <tr class="table-row">
         <td data-label="Date">2014. 11. 11.</td>
         <td data-label="Student">László Kabódi</td>
         <td data-label="Type"><span class="type tdk">TDK</span></td>
-        <td data-label="Title"><a href="https://quszit.github.io/theses/kabodi-2014-kvantumszimulacio-gyorsitas-tdk.pdf" target="_blank">Kvantumáramkör-szimulációs rendszer gyorsításának vizsgálata</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/theses/kabodi-2014-kvantumszimulacio-gyorsitas-tdk.pdf">Kvantumáramkör-szimulációs rendszer gyorsításának vizsgálata</a></td>
       </tr>
     </tbody>
   </table>
