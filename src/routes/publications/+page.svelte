b237d8e3c31da233817b684baf3e41f8b9a0271d
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-08-29 21:35:50 +0200

    add

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -22,7 +22,7 @@
     <tbody>
       <tr class="table-row">
         <td data-label="Authors">Daróczy, Friedl, Kabódi, Pereszlényi, Szabó</td>
-        <td data-label="Title"><a href="https://arxiv.org/abs/2102.00949" target="_blank">Quantum Inspired Adaptive Boosting</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/dfkpsz-2021-quantum-adaptive-boosting.pdf" target="_blank">Quantum inspired adaptive boosting</a></td>
         <td data-label="Year">2021</td>
       </tr>
       
@@ -34,37 +34,37 @@
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://doi.org/10.14232/actacyb.23.2.2017.5" target="_blank">Storing the Quantum Fourier Operator in the QuIDD Data Structure</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-store-qft-quidd.pdf" target="_blank">Storing the quantum Fourier operator in the QuIDD data structure</a></td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title">Embedding logical functions into the Chimera graph</td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-embedding-chimera.pdf" target="_blank">Embedding logical functions into the Chimera graph</a></td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://doi.org/10.3311/PPee.8214" target="_blank">An idea to improve QuIDD based quantum simulations</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2015-improve-quidd.pdf" target="_blank"> An idea to improve QuIDD based quantum simulations</a></td>
         <td data-label="Year">2015</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0211091" target="_blank">Hidden translation and translating coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2014-hidden-translation-translating-coset.pdf" target="_blank">Hidden Translation and Translating Coset in quantum computing</a></td>
         <td data-label="Year">2014</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0208184" target="_blank">Quantum testers for hidden group properties</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fmss-2009-quantum-group-testers.pdf" target="_blank">Quantum testers for hidden group properties</a></td>
         <td data-label="Year">2009</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0211091" target="_blank">Hidden translation and orbit coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2003-hidden-translation-orbit-coset.pdf" target="_blank">Hidden Translation and Orbit Coset in quantum computing</a></td>
         <td data-label="Year">2003</td>
       </tr>
       
