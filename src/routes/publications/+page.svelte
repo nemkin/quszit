c517365b468bb34de8ba10b79cdbd157d53c6fcd
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-09-02 17:29:41 +0200

    add

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -22,25 +22,25 @@
     <tbody>
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2025-qubo-spanning-tree.pdf">QUBO formulation of some spanning tree related problems</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2025-qubo-spanning-tree.pdf">QUBO formulation of some spanning tree related problems</a> (appeared at <a href="https://alg.c.titech.ac.jp/hj2025/">HJ2025</a>)</td>
         <td data-label="Year">2025</td>
       </tr>
 
       <tr class="table-row">
         <td data-label="Authors">Nemkin, Alghazali, Cinkler, Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/nacfk-2025-dwave.pdf">Experiments with QUBO on D-Wave</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/nacfk-2025-dwave.pdf">Experiments with QUBO on D-Wave</a> (appeared at <a href="https://alg.c.titech.ac.jp/hj2025/">HJ2025</a>)</td>
         <td data-label="Year">2025</td>
       </tr>
 
       <tr class="table-row">
         <td data-label="Authors">Friedl, Nemkin</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-nemkin-2023-qwalks.pdf">Simulations of quantum walks on regular graphs</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-nemkin-2023-qwalks.pdf">Simulations of quantum walks on regular graphs</a> (appeared at <a href="http://cs.bme.hu/jh2023/">JH2023</a>)</td>
         <td data-label="Year">2023</td>
       </tr>
 
       <tr class="table-row">
         <td data-label="Authors">Daróczy, Friedl, Kabódi, Pereszlényi, Szabó</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/dfkpsz-2021-quantum-adaptive-boosting.pdf">Quantum inspired adaptive boosting</a></td>
+        <td data-label="Title"><a href="https://arxiv.org/abs/2102.00949">Quantum inspired adaptive boosting</a></td>
         <td data-label="Year">2021</td>
       </tr>
       
@@ -52,37 +52,37 @@
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-store-qft-quidd.pdf">Storing the quantum Fourier operator in the QuIDD data structure</a></td>
+        <td data-label="Title"><a href="https://cyber.bibl.u-szeged.hu/index.php/actcybern/article/view/3938">Storing the quantum Fourier operator in the QuIDD data structure</a></td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-embedding-chimera.pdf">Embedding logical functions into the Chimera graph</a></td>
+        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2017-embedding-chimera.pdf">Embedding logical functions into the Chimera graph</a> (appeared at <a href="http://cs.bme.hu/jh2017/" title="10th Japanese-Hungarian Symposium on Discrete Mathematics and Its Applications, May 22-25, 2017 in Budapest, Hungary">JH2017</a>)</td>
         <td data-label="Year">2017</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Kabódi</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/friedl-kabodi-2015-improve-quidd.pdf"> An idea to improve QuIDD based quantum simulations</a></td>
+        <td data-label="Title"><a href="https://pp.bme.hu/eecs/article/view/8214"> An idea to improve QuIDD based quantum simulations</a></td>
         <td data-label="Year">2015</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2014-hidden-translation-translating-coset.pdf">Hidden Translation and Translating Coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0211091">Hidden Translation and Translating Coset in quantum computing</a></td>
         <td data-label="Year">2014</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fmss-2009-quantum-group-testers.pdf">Quantum testers for hidden group properties</a></td>
+        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0208184">Quantum testers for hidden group properties</a></td>
         <td data-label="Year">2009</td>
       </tr>
       
       <tr class="table-row">
         <td data-label="Authors">Friedl, Ivanyos, Magniez, Santha, Sen</td>
-        <td data-label="Title"><a href="https://quszit.github.io/publications/fimss-2003-hidden-translation-orbit-coset.pdf">Hidden Translation and Orbit Coset in quantum computing</a></td>
+        <td data-label="Title"><a href="https://arxiv.org/abs/quant-ph/0211091">Hidden Translation and Orbit Coset in quantum computing</a></td>
         <td data-label="Year">2003</td>
       </tr>
       
