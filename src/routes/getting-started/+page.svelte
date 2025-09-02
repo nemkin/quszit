8e90ba76040fb01032f4f58784efde87d246bb2b
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-09-02 17:16:12 +0200

    add

--- src/routes/getting-started/+page.svelte
+++ src/routes/getting-started/+page.svelte
@@ -12,9 +12,20 @@
 
     <p>A collection of some resources to get started in quantum computing and quantum algorithms.</p>
 
+    <h3>Talks</h3>
+    
+    An <a href="http://cs.bme.hu/~friedl/elte-summer.pdf">introductory talk on quantum algorithms</a> by Katalin Friedl, presented at <a href="https://www.math.elte.hu/summerschool/?year=2025&page=main">ELTE Summer School in Mathematics</a>.
+
+    <h3>Books</h3>
+    <ul>
+      <li>Mika Hirvensalo: <a href="https://link.springer.com/book/10.1007/978-3-662-09636-9">Quantum Computing</a></li>
+      <li>Sándor Imre, Ferenc Balázs: <a href="https://doi.org/10.1002/9780470869048">Quantum Computing and Communications: An Engineering Approach</a></li>
+      <li>Michael A. Nielsen, Isaac L. Chuang: <a href="https://dl.acm.org/doi/10.5555/1972505">Quantum Computation and Quantum Information</a></li>
+    </ul>
+
     <h3>Courses</h3>
     
-    <p>BME VIK HIT offers several introductory courses on quantum computing (some also available in English), for example:</p>
+    <p>Another department, BME VIK HIT offers several introductory courses on quantum computing (some also available in English), for example:</p>
     <ul>
       <li><a href="https://portal.vik.bme.hu/kepzes/targyak/VIHIMA18/">Kvantuminformatika és -kommunikáció</a>,</li>
       <li><a href="https://portal.vik.bme.hu/kepzes/targyak/VIHIAV06/">Introduction to Quantum Computing and Communication</a>,</li>
@@ -26,13 +37,6 @@
       <li>András Gilyén's <a href="http://gilyen.hu/teaching/QC_2025.html">Quantum Computing</a> at ELTE,</li>
       <li>Mihály Weiner's <a href="https://www.ait-budapest.com/syllabuses/quantum-probability-and-quantum-logic">Quantum Probability and Quantum Logic</a> at AIT,</li>
       <li>Milán Mosonyi's <a href="https://www.ttk.bme.hu/BMETEAOBsMBKIE-00">Introduction to Quantum Information Theory</a> at BME TTK.</li>
-    </ul>
-
-    <h3>Books</h3>
-    <ul>
-      <li>Mika Hirvensalo: <a href="https://link.springer.com/book/10.1007/978-3-662-09636-9">Quantum Computing</a></li>
-      <li>Sándor Imre, Ferenc Balázs: <a href="https://doi.org/10.1002/9780470869048">Quantum Computing and Communications: An Engineering Approach</a></li>
-      <li>Michael A. Nielsen, Isaac L. Chuang: <a href="https://dl.acm.org/doi/10.5555/1972505">Quantum Computation and Quantum Information</a></li>
     </ul>
 
     <h3>Further</h3>
