dfb286979606b8b4fd9a9c572d87ba24d0873d3d
nemkin <viktoria.nemkin@gmail.com>
2025-09-22 09:34:22 +0000

    Publish build from 54fa4bbd08246a31b57c715a591014723daf683e

--- _app/immutable/chunks/Brt-yI7s.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1yp9a9d?.base??"/quantum",a=globalThis.__sveltekit_1yp9a9d?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/LT3k7kXc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_15uipvf?.base??"/quantum",a=globalThis.__sveltekit_15uipvf?.assets??s;export{a,s as b};
