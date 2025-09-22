019de0f0733b82cdc45735a934ba5c0b24ba5210
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-09-22 11:47:39 +0200

    add

--- src/routes/topics/+page.svelte
+++ src/routes/topics/+page.svelte
@@ -19,7 +19,7 @@
 
   One of the most interesting capabilities of the quantum Turing machine is its ability to follow exponentially many computational paths simultaneously. However, the difficulty lies in extracting the result (e.g. whether there is an accepting computational path) due to the model's (and physics') rules. This new model requires ideas and techniques different from the usual algorithmic solutions. Its power is demonstrated, for example, by the fact that a quantum Turing machine can find the prime factors of a number in polynomial time (no such algorithm is known for classical Turing machines), and that searching among <Math tex={m1}/> unordered elements can be done in <Math tex={m2}/> time. The main question in the field is which types of tasks admit to a significantly more efficient quantum algorithmic solution and which are the ones the technique does not help.
 
-  The goal of the research is to examine existing algorithmic methods, develop them further, and investigate their applicability. For example to give quantum algorithmic equivalents to classical algorithms for bipartite matching, spanning trees, or network flows, and determining the extent to which they can be accelerated.
+  The goal of the research is to examine existing algorithmic methods, develop them further, and investigate their applicability. For example to give quantum algorithmic equivalents to classical algorithms for bipartite matching, spanning trees, or network flows, and to determine the extent to which they can be accelerated.
 
   <h3>Quadratic optimization on the adiabatic quantum computer</h3>
 
@@ -31,7 +31,7 @@
 
   Classical random walks on graphs are used in numerous applications. They model random processes and can help solve difficult problems in algorithm theory, that can be formulated as searches. Introducing randomness can provide faster (approximate) results and can be suitable for more efficient processing of larger inputs. For example, the "Boolean satisfiability problem", or SAT for short, is a well-known NP-complete problem, which can be formulated as a search: We can imagine all possible assignments of values to variables as the search space, i.e., the vertices of the graph. Then a possible step (an edge in the graph) could be the negation of a single variable's value. By stepping through this graph based on clever rules, an approximate solution can be found more quickly than with completely randomly generated candidates, and such solvers can perform well in practice.
 
-  There are several methods for quantizing classical walks, that can be interesting for further studying as they exhibit properties significantly different from their classical counterparts. Additionally, Grover's quantum search algorithm can also be understood as a special type of quantum walk.
+  There are several methods for quantizing classical walks, that can be interesting for further study as they exhibit properties significantly different from their classical counterparts. Additionally, Grover's quantum search algorithm can also be understood as a special type of quantum walk.
 
   </div>
 </Table>
